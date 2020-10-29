ddb4a0edfeae37ae79c95819ef704322494fda16
CastilloMatiasAgustin <71848488+CastilloMatiasAgustin@users.noreply.github.com>
2020-10-29 17:03:39 -0300

    Add files via upload

---
+++ BomberPanFinal/Bomber.cpp
@@ -0,0 +1,19 @@
+#include "SFML/Graphics.hpp"
+#include "Menu.h"
+#include <iostream> 
+
+using namespace std;
+using namespace sf;
+
+int main(int argc, char* args[])
+{
+	Controladora* Menu1;
+	Menu1= new Controladora ( 900,  700, "Bomber Pan");
+
+
+
+
+
+
+	return 0;
+}
